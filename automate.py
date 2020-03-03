b0eca1a2c3581bf054a4a08f4a975f32d130452a
soumyajit4419 <ashish454570@gmail.com>
2020-03-03 22:53:05 +0530

    Added doc send

--- automate.py
+++ automate.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium import webdriver
 
 driver = webdriver.Chrome('/home/s0umyajit/Downloads/chromedriver')
@@ -12,12 +14,47 @@ def send_msg():
     count = int(count)
     user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
     user.click()
-
     msg_box = driver.find_element_by_class_name('_3u328')
     for i in range(count):
         msg_box.send_keys(msg)
         btn = driver.find_element_by_class_name('_3M-N-')
         btn.click()
+    return "Message Sent Successfully"
 
 
-send_msg()
+def send_doc():
+    name = input("Enter the name of group or user\t")
+    doc = input("Enter the path of the doc\t")
+    user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
+    user.click()
+
+    attach_box = driver.find_element_by_xpath('//div[@title="Attach"]')
+    attach_box.click()
+
+    document_box = driver.find_element_by_xpath('//input[@accept="*"]')
+    document_box.send_keys(doc)
+    sleep(6)
+    btn = driver.find_element_by_xpath('//span[@data-icon="send-light"]')
+    btn.click()
+    return "Document Sent Successfully"
+
+
+if __name__ == "__main__":
+    try:
+        k = 1
+        while k:
+            print("Enter 1 to send message")
+            print("Enter 2 to send doc")
+            print("Enter 3 to quit")
+            x = int(input("Enter your choice\n"))
+            if x == 1:
+                y = send_msg()
+                print(y)
+            if x == 2:
+                y = send_doc()
+                print(y)
+            if x == 3:
+                exit()
+
+    except Exception as e:
+        print(e)
