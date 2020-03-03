2452a33c79f6d65d6932b8159b4068b0b90fdf6b
soumyajit4419 <ashish454570@gmail.com>
2020-03-04 00:01:00 +0530

    updated image and readme

--- automate.py
+++ automate.py
@@ -39,13 +39,31 @@ def send_doc():
     return "Document Sent Successfully"
 
 
+def send_img():
+    name = input("Enter the name of the user or group\t")
+    image = input("Enter the path of the image/video\t")
+    user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
+    user.click()
+
+    attach_box = driver.find_element_by_xpath('//div[@title="Attach"]')
+    attach_box.click()
+
+    image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+    image_box.send_keys(image)
+    sleep(6)
+    btn = driver.find_element_by_class_name('_1g8sv')
+    btn.click()
+    return "Image Sent Successfully"
+
+
 if __name__ == "__main__":
     try:
         k = 1
         while k:
             print("Enter 1 to send message")
             print("Enter 2 to send doc")
-            print("Enter 3 to quit")
+            print("Enter 3 to send image and videos")
+            print("Enter 4 to quit")
             x = int(input("Enter your choice\n"))
             if x == 1:
                 y = send_msg()
@@ -54,6 +72,9 @@ if __name__ == "__main__":
                 y = send_doc()
                 print(y)
             if x == 3:
+                y = send_img()
+                print(y)
+            if x == 4:
                 exit()
 
     except Exception as e:
